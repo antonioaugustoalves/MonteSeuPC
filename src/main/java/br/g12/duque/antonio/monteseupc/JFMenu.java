db9c7e44ee987a85865bd778eaa701fadfcbf901
Antonio Alves <antonioaugusto.alves1981@gmail.com>
2023-08-24 21:46:07 -0300

    Variaveis para clacular o preco do componente

--- src/main/java/br/g12/duque/antonio/monteseupc/JFMenu.java
+++ src/main/java/br/g12/duque/antonio/monteseupc/JFMenu.java
@@ -11,11 +11,11 @@ package br.g12.duque.antonio.monteseupc;
 public class JFMenu extends javax.swing.JFrame {
 
     String [] intel = {
-        "Intel Core i9-12900K",
-        "Intel Core i7-12700K",
-        "Intel Core i5-12600K",
-        "Intel Core i9-11900K",
-        "Intel Core i5-11600K"
+        "Intel Core i9-12900K",//0
+        "Intel Core i7-12700K",//1
+        "Intel Core i5-12600K",//2
+        "Intel Core i9-11900K",//3
+        "Intel Core i5-11600K"//4
     };
     
     String [] amd = {
@@ -27,10 +27,41 @@ public class JFMenu extends javax.swing.JFrame {
             
     };
     
+    double[] precosIntel = {
+        800.00, 
+        1200.00, 
+        1400.00,
+        1600.00,
+        2000.00
+    };
+    
+    double[] precosAMD = {
+        900.00, 
+        1300.00, 
+        1500.00,
+        1700.00,
+        2100.00
+    };
+    
+    double precoCPU = 0;
+    double precoPlacaMae = 0;
+    double precoMemoria = 0;
+    double precoGPU = 0;
+    double precoSSD = 0;
+    
     
     
     public JFMenu() {
         initComponents();
+        rbIntel.setSelected(true);
+        rbGigabyte.setSelected(true);
+        
+        // Preencher o combo box com os dados da intel
+        cbxProcessador.removeAllItems();
+        cbxProcessador.addItem("Selecione o processador");
+        for(int i = 0; i < 5; i++){
+            cbxProcessador.addItem(intel[i]);
+        }
     }
 
     /**
@@ -51,9 +82,9 @@ public class JFMenu extends javax.swing.JFrame {
         cbxProcessador = new javax.swing.JComboBox<>();
         jPanel1 = new javax.swing.JPanel();
         jLabel3 = new javax.swing.JLabel();
-        jRadioButton1 = new javax.swing.JRadioButton();
-        jRadioButton2 = new javax.swing.JRadioButton();
-        jRadioButton3 = new javax.swing.JRadioButton();
+        rbGigabyte = new javax.swing.JRadioButton();
+        rbAsus = new javax.swing.JRadioButton();
+        rbAsRock = new javax.swing.JRadioButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
@@ -63,9 +94,19 @@ public class JFMenu extends javax.swing.JFrame {
 
         grpProcessador.add(rbIntel);
         rbIntel.setText("Intel");
+        rbIntel.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                rbIntelActionPerformed(evt);
+            }
+        });
 
         grpProcessador.add(rbAMD);
         rbAMD.setText("AMD");
+        rbAMD.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                rbAMDActionPerformed(evt);
+            }
+        });
 
         jLabel2.setFont(new java.awt.Font("Dialog", 1, 12)); // NOI18N
         jLabel2.setForeground(new java.awt.Color(204, 0, 0));
@@ -80,14 +121,14 @@ public class JFMenu extends javax.swing.JFrame {
         jLabel3.setForeground(new java.awt.Color(255, 255, 255));
         jLabel3.setText("Selecione a placa-mãe:");
 
-        grpPlacaMae.add(jRadioButton1);
-        jRadioButton1.setText("Gigabyte");
+        grpPlacaMae.add(rbGigabyte);
+        rbGigabyte.setText("Gigabyte");
 
-        grpPlacaMae.add(jRadioButton2);
-        jRadioButton2.setText("ASUS");
+        grpPlacaMae.add(rbAsus);
+        rbAsus.setText("ASUS");
 
-        grpPlacaMae.add(jRadioButton3);
-        jRadioButton3.setText("AsRock");
+        grpPlacaMae.add(rbAsRock);
+        rbAsRock.setText("AsRock");
 
         javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
         jPanel1.setLayout(jPanel1Layout);
@@ -96,9 +137,9 @@ public class JFMenu extends javax.swing.JFrame {
             .addGroup(jPanel1Layout.createSequentialGroup()
                 .addContainerGap()
                 .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addComponent(jRadioButton3, javax.swing.GroupLayout.PREFERRED_SIZE, 91, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(jRadioButton2, javax.swing.GroupLayout.PREFERRED_SIZE, 91, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(jRadioButton1, javax.swing.GroupLayout.PREFERRED_SIZE, 91, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(rbAsRock, javax.swing.GroupLayout.PREFERRED_SIZE, 91, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(rbAsus, javax.swing.GroupLayout.PREFERRED_SIZE, 91, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(rbGigabyte, javax.swing.GroupLayout.PREFERRED_SIZE, 91, javax.swing.GroupLayout.PREFERRED_SIZE)
                     .addComponent(jLabel3, javax.swing.GroupLayout.PREFERRED_SIZE, 152, javax.swing.GroupLayout.PREFERRED_SIZE))
                 .addContainerGap(193, Short.MAX_VALUE))
         );
@@ -108,11 +149,11 @@ public class JFMenu extends javax.swing.JFrame {
                 .addGap(6, 6, 6)
                 .addComponent(jLabel3)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
-                .addComponent(jRadioButton1)
+                .addComponent(rbGigabyte)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
-                .addComponent(jRadioButton2)
+                .addComponent(rbAsus)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
-                .addComponent(jRadioButton3)
+                .addComponent(rbAsRock)
                 .addContainerGap(17, Short.MAX_VALUE))
         );
 
@@ -122,15 +163,15 @@ public class JFMenu extends javax.swing.JFrame {
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
                 .addContainerGap()
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addComponent(jLabel1, javax.swing.GroupLayout.PREFERRED_SIZE, 193, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
                     .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING, false)
                         .addComponent(cbxProcessador, javax.swing.GroupLayout.Alignment.LEADING, 0, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                         .addGroup(layout.createSequentialGroup()
                             .addComponent(rbIntel, javax.swing.GroupLayout.PREFERRED_SIZE, 91, javax.swing.GroupLayout.PREFERRED_SIZE)
                             .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                             .addComponent(rbAMD, javax.swing.GroupLayout.PREFERRED_SIZE, 91, javax.swing.GroupLayout.PREFERRED_SIZE)))
-                    .addComponent(jLabel2))
+                    .addComponent(jLabel2, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                    .addComponent(jLabel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                 .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addContainerGap(335, Short.MAX_VALUE))
@@ -156,6 +197,25 @@ public class JFMenu extends javax.swing.JFrame {
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
+
+    private void rbAMDActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_rbAMDActionPerformed
+        // TODO add your handling code here:
+        cbxProcessador.removeAllItems();
+        cbxProcessador.addItem("Selecione o processador");
+        for(int i = 0; i < 5; i++){
+            cbxProcessador.addItem(amd[i]);
+        }
+    }//GEN-LAST:event_rbAMDActionPerformed
+
+    private void rbIntelActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_rbIntelActionPerformed
+        // TODO add your handling code here:
+        cbxProcessador.removeAllItems();
+        cbxProcessador.addItem("Selecione o processador");
+        for(int i = 0; i < 5; i++){
+            cbxProcessador.addItem(intel
+                    [i]);
+        }
+    }//GEN-LAST:event_rbIntelActionPerformed
 
     /**
      * @param args the command line arguments
@@ -200,10 +260,10 @@ public class JFMenu extends javax.swing.JFrame {
     private javax.swing.JLabel jLabel2;
     private javax.swing.JLabel jLabel3;
     private javax.swing.JPanel jPanel1;
-    private javax.swing.JRadioButton jRadioButton1;
-    private javax.swing.JRadioButton jRadioButton2;
-    private javax.swing.JRadioButton jRadioButton3;
     private javax.swing.JRadioButton rbAMD;
+    private javax.swing.JRadioButton rbAsRock;
+    private javax.swing.JRadioButton rbAsus;
+    private javax.swing.JRadioButton rbGigabyte;
     private javax.swing.JRadioButton rbIntel;
     // End of variables declaration//GEN-END:variables
 }
